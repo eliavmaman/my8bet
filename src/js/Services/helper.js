b73c814748d3d39217759a9d0730a89f87047718
eliav <eliavmaman2@gmail.com>
2018-12-27 18:05:09 +0200

    changed user verification from sessionstorage, added AI events and change kambi cdn when working local

--- src/js/Services/helper.js
+++ src/js/Services/helper.js
@@ -1,40 +1,30 @@
 export function getCID() {
 
-    for (var i = 0; i < localStorage.length; i++) {
-        if(!isJson(localStorage.key(i)))
-            continue;
-        var key = JSON.parse(localStorage.getItem(localStorage.key(i)));
-
-        if (typeof key.named_user_id !== "undefined")
-            return key.named_user_id;
-
+    if (!sessionStorage.CID) {
+        return 123;
     }
-    return null;
+
+    return sessionStorage.CID;
 }
 
 
 export function getCIDOrDefault() {
 
-    for (var i = 0; i < localStorage.length; i++) {
-        if(!isJson(localStorage.key(i)))
-            continue;
-        var key = JSON.parse(localStorage.getItem(localStorage.key(i)));
-
-        if (typeof key.named_user_id !== "undefined")
-            return key.named_user_id;
-
+    if (!sessionStorage.CID) {
+        return 123;
     }
-    return 123;
+
+    return sessionStorage.CID;
 }
 
 export function saveUserToLocalStorage(user) {
 
-    localStorage.setItem('8bet-user', JSON.stringify(user));
+    sessionStorage.setItem('8bet-user', JSON.stringify(user));
 }
 
 export function getUserFromLocalStorage() {
 
-    return JSON.parse(localStorage.getItem('8bet-user'));
+    return JSON.parse(sessionStorage.getItem('8bet-user'));
 }
 
 export function isUserSubscribeToLiveEvents() {
@@ -42,10 +32,4 @@ export function isUserSubscribeToLiveEvents() {
     let user = getUserFromLocalStorage(cid);
 
     return user ? user.settings.liveEvents : false;
-}
-function isJson(content){
-    return  (/^[\],:{}\s]*$/.test(content.replace(/\\["\\\/bfnrtu]/g, '@').
-    replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
-    replace(/(?:^|:|,)(?:\s*\[)+/g, '')))
-
 }
--- src/js/Services/kambi.js
+++ src/js/Services/kambi.js
@@ -147,7 +147,7 @@ const getTeamsByName = function (name) {
         //return axios.get(`https://cts-api.kambi.com/offering/api/v3/888/term/search.json?lang=en_GB&market=zz&client_id=${config.client_id}&chnnael_id=${config.channelId}&ncid=1545580760169&term=${name}`);
     } else {
         // return axios.get(`https://cts-api.kambi.com/offering/api/v3/888/term/search.json?lang=en_GB&market=${config.market}&client_id=${config.client_id}&chnnael_id=${config.channelId}&ncid=1545580760169&term=${name}`);
-        return axios.get('https://cts-api.kambi.com/offering/api/v3/888/term/search.json?lang=en_GB&market=ZZ&client_id=2&chnnael_id=1&ncid=1545580760169&term=' + name);
+        return axios.get('https://cts-api.kambi.com/offering/api/v3/888/term/search.json?lang=en_GB&market=GB&client_id=2&chnnael_id=1&ncid=1545580760169&term=' + name);
     }
 };
 
